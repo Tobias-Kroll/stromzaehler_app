20fdef90b1b1671e44122926a1348f3c0fd20c70
Tobias Kroll <tobias.kroll2@stud.htw-dresden.de>
2025-08-22 22:07:45 +0200

    Tasks von M3 abgearbeitet

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'package:fl_chart/fl_chart.dart';
 
 import 'package:flutter/material.dart';
 
@@ -103,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
               width: double.infinity, // volle Breite
               height: double.infinity, // volle Höhe
               color: Colors.transparent,
-              margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+              margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
 //----------------------------------------------------
 //    4.3.1.1 genutze Spalte im Body Conatiner
               child: Column(
@@ -133,6 +134,72 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ],
                 ),
+
+                    //---------------
+                    //  Text inhalt im oberen Container
+                    child: Container(
+                      width: double.infinity, // volle Breite
+                      height: double.infinity,
+
+
+                      child: Column(
+
+                        children: [
+
+                          // Box um Übersicht
+                          Container(
+                            margin: EdgeInsets.only(top: 0, left: 0, right: 0),
+                            height: 80,
+                            alignment: Alignment.center,
+
+
+                            decoration: BoxDecoration(
+                              color: Colors.lightBlue[50],
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                color: Colors.transparent, // Ränder unsichtbar
+                                width: 2,
+                            ),
+                            ),
+                            child:  Text('Verbrauchsübersicht',
+                                    style: TextStyle(fontSize: 25),
+                           ),
+                          ),
+                          const SizedBox(height: 12),
+
+
+                          Container(
+                            width: double.infinity,
+                            padding: const EdgeInsets.symmetric(horizontal: 12),
+                            child : Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                //Texte links
+                                Text('Kosten : ≈ 300 €',
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                                SizedBox (height: 6),
+                                Text('Verbauch : 350 kWh',
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                                SizedBox (height: 6),
+                                Text('Turnusende in : 4 Monaten',
+                                      style: TextStyle(fontSize: 20),
+                                ),
+                                SizedBox( height: 6),
+                                Text('Monatsdurchschnitt : 40 kWh',
+                                      style: TextStyle(fontSize: 20),
+                                )
+                              ],
+                            ),
+                          ),
+
+
+                        ],
+
+                      ),
+                    ),
+
                   ),
                   ),
 //-----------------------------------------------------
@@ -160,7 +227,138 @@ class _MyHomePageState extends State<MyHomePage> {
                             offset: Offset(0, 3)
                         ),
                       ],
+                    ), 
+                    
+                    child: Container(
+                      margin: EdgeInsetsDirectional.fromSTEB(15, 15, 15, 5),
+                    child: BarChart(
+                  BarChartData(
+                  borderData: FlBorderData(
+                    show: false,
+                  ),
+                    gridData: FlGridData(
+                      show: false,
                     ),
+                    barGroups: [
+                      BarChartGroupData(
+                        x: 1,
+                        barRods:[
+                          BarChartRodData(toY: 10, color: Colors.lightBlue[200]),
+                        ],
+                      ),
+                      BarChartGroupData(
+                        x: 2,
+                        barRods:[
+                          BarChartRodData(toY: 20, color: Colors.lightBlue[200]),
+                        ],
+                      ),
+                      BarChartGroupData(
+                        x: 3,
+                        barRods:[
+                          BarChartRodData(toY: 12, color: Colors.lightBlue[200]),
+                        ],
+                      ),
+                      BarChartGroupData(
+                        x: 4,
+                        barRods:[
+                          BarChartRodData(toY: 10, color: Colors.lightBlue[200]),
+                        ],
+                      ),
+                      BarChartGroupData(
+                        x: 5,
+                        barRods:[
+                          BarChartRodData(toY: 10, color: Colors.lightBlue[200]),
+                        ],
+                      ),
+                      BarChartGroupData(
+                        x: 6,
+                        barRods:[
+                          BarChartRodData(toY: 10, color: Colors.lightBlue[200]),
+                        ],
+                      ),
+                      BarChartGroupData(
+                        x: 7,
+                        barRods:[
+                          BarChartRodData(toY: 10, color: Colors.lightBlue[200]),
+                        ],
+                      ),
+                      BarChartGroupData(
+                        x: 8,
+                        barRods:[
+                          BarChartRodData(toY: 10, color: Colors.lightBlue[200]),
+                        ],
+                      ),
+                      BarChartGroupData(
+                        x: 9,
+                        barRods:[
+                          BarChartRodData(toY: 10, color: Colors.lightBlue[200]),
+                        ],
+                      ),
+                      BarChartGroupData(
+                        x: 10,
+                        barRods:[
+                          BarChartRodData(toY: 10, color: Colors.lightBlue[200]),
+                        ],
+                      ),
+                      BarChartGroupData(
+                        x: 11,
+                        barRods:[
+                          BarChartRodData(toY: 10, color: Colors.lightBlue[200]),
+                        ],
+                      ),
+                      BarChartGroupData(
+                        x: 12,
+                        barRods:[
+                          BarChartRodData(toY: 10, color: Colors.lightBlue[200]),
+                        ],
+                      ),
+
+                    ],
+                    alignment: BarChartAlignment.spaceAround,
+                    titlesData: FlTitlesData(
+                      show: true, //Beschriftung der Achsen an oder aus
+                      leftTitles :  AxisTitles(
+                        sideTitles: SideTitles(
+                          reservedSize: 40,
+                          showTitles: false,
+                        ),
+                      ),
+
+                      rightTitles :  AxisTitles(
+                        sideTitles: SideTitles(
+                          reservedSize: 40,
+                          showTitles: false,
+                        ),
+                      ),
+
+                      topTitles :  AxisTitles(
+                        sideTitles: SideTitles(
+                          reservedSize: 40,
+                          showTitles: false,
+                        ),
+                      ),
+
+                      bottomTitles :  AxisTitles(
+                        axisNameWidget: Text('Monatlicher Turnusverlauf'),
+                        axisNameSize: 20,
+                        // drawBelowEverything: false,
+                        sideTitles: SideTitles(
+                          reservedSize: 40,
+                          showTitles: true,
+                        ),
+                      ),
+
+                    ),
+                    // rangeAnnotations hinzufügen für Durschnittslinie
+                    backgroundColor: Colors.transparent,
+                    barTouchData: BarTouchData(// Berühren des Charts zeigt Informationen
+                      enabled: false,
+                    ),
+                  ),
+
+      ),
+                  ),
+
                   ),
                   ),
                 ],
