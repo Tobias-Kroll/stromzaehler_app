36c98ad97afad86ad7e2a2a21f1a9da61fce9faf
Tobias Kroll <tobias.kroll2@stud.htw-dresden.de>
2025-08-29 19:55:22 +0200

    Zweite Seite Design Grundgerüst

--- lib/eingaben_page.dart
+++ lib/eingaben_page.dart
@@ -37,11 +37,87 @@ class EingabenSeite extends StatelessWidget {
         backgroundColor: Colors.transparent,
         title: const Text("Einageben"),
       ),
-      body: const Center(
-        child: Text("Hier kommen die Felder"),
+      body: Container(
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+
+      child: CustomScrollView(
+        //controller: scrollCtrl,
+        slivers: [
+          SliverToBoxAdapter(
+           child: Container(
+             decoration: BoxDecoration(
+               border: Border.all(
+                 color: Colors.black,
+                 width: 2,
+               ),
+               borderRadius: BorderRadius.circular(12),
+             ),
+             //height: ,
+               margin: EdgeInsets.symmetric(horizontal: 10),
+               child: Column(
+                 children: [
+                    Text('Turnusregelung'),
+                    SizedBox(height: 20,),
+                    ExpansionTile(
+                       title: Text('Aufklappbar'),
+                      children: List.generate(
+                      20,
+                          (i) => ListTile(
+                        leading: Icon(Icons.bolt),
+                        title: Text('Eintrag $i'),
+                        subtitle: Text('Detail zu Eintrag $i'),
+                      ),
+                    ),
+                   ),
+                 ],
+               ),
+           ),
+          ),
+
+          SliverToBoxAdapter(
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  width: 2,
+                ),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              height: 200,
+            ),
+          ),
+         
+          SliverToBoxAdapter(
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  width: 2,
+                ),
+                borderRadius: BorderRadius.circular(12),
+              ),
+
+              height: 150,
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                children: [
+                  Text('Zählerstande'),
+                  SizedBox(height: 20,),
+                  ExpansionTile(
+                      title: Text('Aufklappbar')
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
-    )
+
+      ),
+      ),
     );
+
   }
 
 }
